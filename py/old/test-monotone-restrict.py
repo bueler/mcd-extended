8dfef097e0d3826285cedf6b3410fc27f2bc2344
Ed Bueler <elbueler@alaska.edu>
2022-07-22 17:50:03 -0800

    go ahead and test injection too

--- py/old/test-monotone-restrict.py
+++ py/old/test-monotone-restrict.py
@@ -33,6 +33,16 @@ def test_restrict():
     File("ff.pvd").write(f_fine)
     File("ff-restricted.pvd").write(f_restricted)
 
+def test_inject():
+    """ inject a function f from fine mesh hierarchy[-1] to coarse mesh hierarchy[0] and write out .pvd for f on each mesh """
+    f_fine = Function(Vf).interpolate(exact)
+    f_injected = Function(Vc)
+    inject(f_fine,f_injected)
+    f_fine.rename("f_fine")
+    f_injected.rename("f_injected")
+    File("fff.pvd").write(f_fine)
+    File("fff-injected.pvd").write(f_injected)
+
 def test_monotone_restrict():
     """ monotone restrict a function f from fine mesh hierarchy[-1] to coarse mesh hierarchy[0] and write out .pvd for f on each mesh; thanks to Lawrence Mitchell for this code; note the negative function f(x,y) = -xy generates nonzero monotone restriction onto coarsest mesh UnitSquareMesh(2, 2) """
     f_fine = Function(Vf).interpolate(- x * y)
@@ -43,9 +53,10 @@ def test_monotone_restrict():
     #print(f_restricted.dat.data_ro)
     f_fine.rename("f_fine")
     f_restricted.rename("f_restricted")
-    File("fff.pvd").write(f_fine)
-    File("fff-mrestricted.pvd").write(f_restricted)
+    File("ffff.pvd").write(f_fine)
+    File("ffff-mrestricted.pvd").write(f_restricted)
 
 test_prolong()
 test_restrict()
+test_inject()
 test_monotone_restrict()
