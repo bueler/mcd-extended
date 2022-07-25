1f9bdb06b8915a8fdabd5502ff890f49f50cd071
Ed Bueler <elbueler@alaska.edu>
2022-07-24 17:02:04 -0800

    bring FAS Poisson into compliance with Bratu 1D choices ... seems to work

--- py/poisson.py
+++ py/poisson.py
@@ -8,32 +8,41 @@ where v is a test function and
 and boundary conditions are homogeneous Dirichlet.
 
 Solver is FAS V-cycles.  Going downward the V-cycle uses solve() for a
-down-smoother, then forms the FAS correction equation, then goes down a level.
+down-smoother, forms the FAS correction equation, and decrements the level.
 The coarsest problem is solved by LU.  Going upward the V-cycle prolongs and
-adds the computed correction and then uses solve() as an up-smoother.  The
-smoothers are a fixed number of SSOR iterations.
+adds the computed correction and then uses solve() as an up-smoother.
+
+The smoother is a KSP-only solve, with two CG+ICC iterations.
 
 The FAS correction equation is
     Fc(uc)[vc] = ellc[vc]
 where Fc has same formula as F and
     ellc[vc] = R'(resf)[vc] + Fc(R(uf))[vc]
 and
-    resf[v] = ellf[v] - Ff(uf)[v]
-and R' is canonical restriction and R is injection.
+    resf[v] = Ff(uf)[v] - ellf[v]
+and R' is canonical restriction and R is injection.  After solution the fine
+level iterate is updated
+    u <- u + P(uc - R(uf))
+where P is prolongation.
 """
 
 from firedrake import *
 
-levels = 2
+levels = 4
 vcycles = 1
 smootherparams = {"snes_type": "ksponly",
                   "snes_max_linear_solve_fail": 2, # don't error when KSP reports DIVERGED_ITS
-                  #"ksp_converged_reason": None,
-                  "ksp_type": "richardson",
-                  "ksp_max_it": 1,
-                  "pc_type": "sor"}
+                  "ksp_converged_reason": None,
+                  "ksp_type": "cg",
+                  #"ksp_rtol": 0.01,
+                  "ksp_max_it": 2,
+                  "pc_type": "icc"}
                   #"ksp_type": "preonly",
                   #"pc_type": "lu"}
+downparams = smootherparams.copy()
+#downparams["pc_sor_forward"] = None
+upparams = smootherparams.copy()
+#upparams["pc_sor_backward"] = None
 coarseparams = {"snes_type": "ksponly",
                 #"ksp_converged_reason": None,
                 "ksp_type": "preonly",
@@ -41,7 +50,6 @@ coarseparams = {"snes_type": "ksponly",
 
 # set up mesh hierarchy
 cmesh = UnitSquareMesh(2, 2)  # as small as practical for nontriviality
-#FIXME try cmesh = UnitIntervalMesh(2) in new 1D code which tries to exactly reproduce fas-intro example
 hierarchy = MeshHierarchy(cmesh, levels-1)
 
 # set up function spaces and some functions on each level
@@ -50,7 +58,10 @@ u = [Function(VV[i]) for i in range(levels)] # values initialized to zero
 v = [TestFunction(VV[i]) for i in range(levels)]
 F = [dot(grad(u[i]), grad(v[i])) * dx for i in range(levels)]
 bcs = [DirichletBC(VV[i], zero(), (1, 2, 3, 4)) for i in range(levels)]
+
+# need to compute the problem form for the restriction of u
 Ru = [Function(VV[i]) for i in range(levels)]
+FRu = [dot(grad(Ru[i]), grad(v[i])) * dx for i in range(levels)]
 
 # blank lists for functions to be created on descent by FAS formulas
 ell = [None for i in range(levels)]
@@ -61,12 +72,20 @@ fine = levels-1
 x, y = SpatialCoordinate(hierarchy[fine])
 f = -0.5*pi*pi*(4*cos(pi*x) - 5*cos(pi*x*0.5) + 2)*sin(pi*y)
 exact = sin(pi*x)*tan(pi*x*0.25)*sin(pi*y)
+
+# evaluate error using function-space norms, L2 or Linf
+useL2 = True
 def error(u):
-    return norm(assemble(u - Function(VV[fine]).interpolate(exact)))
+    udiff = Function(VV[fine]).interpolate(u - exact)
+    if useL2:
+        return sqrt(assemble(dot(udiff, udiff) * dx))
+    else:
+        with udiff.dat.vec_ro as vudiff:
+            return abs(vudiff).max()[1]
 
 # fine-mesh problem
 ell[fine] = f * v[fine] * dx
-res[fine] = ell[fine] - F[fine]
+res[fine] = F[fine] - ell[fine]
 
 print('initial             |residual|=%.6f  |error|=%.6f' \
       % (norm(assemble(res[fine])), error(u[fine])))
@@ -75,8 +94,8 @@ for j in range(vcycles):
     # down-smoothing and creation of FAS correction equation
     for i in range(levels-1,0,-1):   # levels-1, ..., 1
         # smoother application
-        solve(res[i] == 0, u[i], bcs=bcs[i],
-              solver_parameters=smootherparams)
+        solve(res[i] == 0, u[i], bcs=bcs[i], options_prefix = 'down%d' % i,
+              solver_parameters=downparams)
         # construct FAS correction problem with right-hand side
         #     ell[i-1] = R'(res(u[i]))) + F[i-1](R(u[i]))
         # where R' is canonical restriction and R is injection
@@ -85,13 +104,12 @@ for j in range(vcycles):
                                          # res[i] is UFL form only
         inject(u[i], Ru[i-1])
         ell[i-1] = Rres * v[i-1] * dx + dot(grad(Ru[i-1]), grad(v[i-1])) * dx
-        res[i-1] = ell[i-1] - F[i-1]
-        inject(u[i], u[i-1])  # for initial value on next level
+        res[i-1] = F[i-1] - ell[i-1]
+        inject(u[i], u[i-1])  # for initial value on next level; since u[i-1]=Ru[i-1],
                               # equivalent to setting initial error iterate to zero
-                              # but restrict() seems better?
 
     # coarse-mesh problem and direct solution
-    solve(res[0] == 0, u[0], bcs=bcs[0],
+    solve(res[0] == 0, u[0], bcs=bcs[0], options_prefix = 'coarse',
           solver_parameters=coarseparams)
     print('%s|coarse cor|=%.6f' % ((levels-1)*'  ', norm(u[0])))
 
@@ -101,12 +119,16 @@ for j in range(vcycles):
         prolong(assemble(u[i-1] - Ru[i-1]), Pcor)  # assemble() needed because
                                                    # subtraction is UFL
         u[i] += Pcor
-        solve(res[i] == 0, u[i], bcs=bcs[i],
-              solver_parameters=smootherparams)
+        solve(res[i] == 0, u[i], bcs=bcs[i], options_prefix = 'up%d' % i,
+              solver_parameters=upparams)
 
     # report on result of cycle
-    print('vcycle %d (%d levels) |residual|=%.6f  |error|=%.6f' \
+    print('FAS V-cycle %d (%d levels) |residual|=%.6f  |error|=%.6f' \
           % (j+1, levels, norm(assemble(res[fine])), error(u[fine])))
 
 u[fine].rename("u")
-File("u-poisson.pvd").write(u[fine])
+uexact = Function(VV[fine]).interpolate(exact)
+uexact.rename("uexact")
+udiff = Function(VV[fine]).interpolate(u[fine] - exact)
+udiff.rename("udiff")
+File("u-poisson.pvd").write(u[fine],uexact,udiff)
