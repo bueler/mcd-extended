d5a711352e57e863fb2321e9c363e6c2b2893d52
Ed Bueler <elbueler@alaska.edu>
2022-09-05 12:13:42 -0800

    regression testing for vanilla Q1 transfer ops

--- c/transfers.c
+++ c/transfers.c
@@ -1,109 +1,67 @@
-FIXME  completely rewrite; this will just test src/q1transfers.h|c; see "#if 1" block below
-
 static char help[] =
-"Test a two-level LDC stack.\n\n";
-
-// FIXME check all results!
+"Test Q1 transfer operators on a two-level LDC stack.  The region is the\n"
+"square [0,1]^2, the fine level is a 5x5 grid and the coarse is a 3x3 grid.\n"
+"The test here only addresses the DMDAs at the two level.  (Defect\n"
+"constraints are not tested; compare ldctwolevel.c.)\n\n";
 
 #include <petsc.h>
 #include "src/q1transfers.h"
 #include "src/ldc.h"
 
-// z = gamma_lower(x,y) is the hemispherical obstacle, but made C^1 with "skirt" at r=r0
+// z = gamma_lower(x,y) has tight bounds  0 <= z <= 1
 PetscReal gamma_lower(PetscReal x, PetscReal y) {
-    const PetscReal  r = PetscSqrtReal(x * x + y * y),
-                     r0 = 0.9,
-                     psi0 = PetscSqrtReal(1.0 - r0 * r0),
-                     dpsi0 = - r0 / psi0;
-    if (r <= r0) {
-        return PetscSqrtReal(1.0 - r * r);
-    } else {
-        return psi0 + dpsi0 * (r - r0);
-    }
+    return 16.0 * x * (1.0 - x) * y * (1.0 - y);
 }
 
 extern PetscErrorCode VecViewMatlabStdout(Vec);
-extern PetscErrorCode FormVecFromFormula(PetscReal (*)(PetscReal,PetscReal),
-                                         DMDALocalInfo*, Vec);
 
 int main(int argc,char **argv) {
-    Vec            w;
     LDC            ldc[2];
+    DMDALocalInfo  info;
+    Vec            vgamlow, vcoarseFW, vcoarseINJ, vfine;
 
     PetscCall(PetscInitialize(&argc,&argv,NULL,help));
 
     // create LDC stack
-    PetscCall(LDCCreateCoarsest(PETSC_TRUE,3,3,-2.0,2.0,-2.0,2.0,&(ldc[0])));
+    PetscCall(LDCCreateCoarsest(PETSC_TRUE,3,3,0.0,1.0,0.0,1.0,&(ldc[0])));
     PetscCall(LDCRefine(&(ldc[0]),&(ldc[1])));
 
-    // view DMDA at each level
-    PetscCall(PetscObjectSetName((PetscObject)(ldc[0].dal),"ldc[0].dal"));
-    PetscCall(PetscObjectSetName((PetscObject)(ldc[1].dal),"ldc[1].dal"));
-    PetscCall(DMView(ldc[0].dal, PETSC_VIEWER_STDOUT_WORLD));
-    PetscCall(DMView(ldc[1].dal, PETSC_VIEWER_STDOUT_WORLD));
+    // create and view a Vec on the fine level
+    PetscCall(DMCreateGlobalVector(ldc[1].dal,&vgamlow));
+    PetscCall(PetscObjectSetName((PetscObject)(vgamlow),"gamlow"));
+    PetscCall(DMDAGetLocalInfo(ldc[1].dal,&info));
+    //PetscCall(FormVecFromFormula(gamma_lower,&info,vgamlow));
+    PetscCall(LDCVecFromFormula(ldc[1],gamma_lower,vgamlow));
+    PetscCall(VecViewMatlabStdout(vgamlow));
 
-#if 1
     // test Q1 restriction, injection, and interpolation on temporary vecs
-    Vec vcoarseFW, vcoarseINJ, vfine;
     PetscCall(DMCreateGlobalVector(ldc[0].dal,&vcoarseFW));
+    PetscCall(PetscObjectSetName((PetscObject)(vcoarseFW),"coarseFW"));
     PetscCall(DMCreateGlobalVector(ldc[0].dal,&vcoarseINJ));
+    PetscCall(PetscObjectSetName((PetscObject)(vcoarseINJ),"coarseINJ"));
     PetscCall(DMCreateGlobalVector(ldc[1].dal,&vfine));
-    PetscCall(Q1Restrict(ldc[1].dal,ldc[0].dal,ldc[1].gamlow,&vcoarseFW));
-    PetscCall(Q1Inject(ldc[1].dal,ldc[0].dal,ldc[1].gamlow,&vcoarseINJ));
+    PetscCall(PetscObjectSetName((PetscObject)(vfine),"fine"));
+    PetscCall(Q1Restrict(ldc[1].dal,ldc[0].dal,vgamlow,&vcoarseFW));
+    PetscCall(Q1Inject(ldc[1].dal,ldc[0].dal,vgamlow,&vcoarseINJ));
     PetscCall(Q1Interpolate(ldc[0].dal,ldc[1].dal,vcoarseFW,&vfine));
-    PetscCall(VecViewMatlabStdout(ldc[1].gamlow));
     PetscCall(VecViewMatlabStdout(vcoarseFW));
     PetscCall(VecViewMatlabStdout(vcoarseINJ));
     PetscCall(VecViewMatlabStdout(vfine));
+
+    // destroy
     PetscCall(VecDestroy(&vfine));
     PetscCall(VecDestroy(&vcoarseINJ));
     PetscCall(VecDestroy(&vcoarseFW));
-#endif
-
-    // iterate w=1 gives up defect constraint on finest level
-    PetscCall(PetscPrintf(PETSC_COMM_WORLD,"using iterate w=1 to generate finest-level up defect constraints\n"));
-    PetscCall(DMCreateGlobalVector(ldc[1].dal,&w));
-    PetscCall(VecSet(w,1.0));
-    PetscCall(LDCFinestUpDefectConstraintsFromFormulas(w,NULL,&gamma_lower,&(ldc[1])));
-    PetscCall(VecDestroy(&w));
-
-    // generate up and down defect constraints for both levels
-    PetscCall(PetscPrintf(PETSC_COMM_WORLD,"generating defect constraints for V-cycle\n"));
-    PetscCall(LDCGenerateDefectConstraintsVCycle(&(ldc[1])));
-
-    // ranges on Vecs on each level
-    PetscCall(LDCReportRanges(ldc[0]));
-    PetscCall(LDCReportRanges(ldc[1]));
-
-    // destroy
+    PetscCall(VecDestroy(&vgamlow));
     PetscCall(LDCDestroy(&(ldc[1])));
     PetscCall(LDCDestroy(&(ldc[0])));
     PetscCall(PetscFinalize());
     return 0;
 }
 
-
 PetscErrorCode VecViewMatlabStdout(Vec v) {
     PetscCall(PetscViewerPushFormat(PETSC_VIEWER_STDOUT_WORLD,PETSC_VIEWER_ASCII_MATLAB));
     PetscCall(VecView(v,PETSC_VIEWER_STDOUT_WORLD));
     PetscCall(PetscViewerPopFormat(PETSC_VIEWER_STDOUT_WORLD));
-    return 0;
-}
-
-PetscErrorCode FormVecFromFormula(PetscReal (*ufcn)(PetscReal,PetscReal),
-                                  DMDALocalInfo *info, Vec u) {
-    PetscInt     i, j;
-    PetscReal    hx, hy, x, y, **au;
-    hx = 4.0 / (PetscReal)(info->mx - 1);
-    hy = 4.0 / (PetscReal)(info->my - 1);
-    PetscCall(DMDAVecGetArray(info->da, u, &au));
-    for (j=info->ys; j<info->ys+info->ym; j++) {
-        y = -2.0 + j * hy;
-        for (i=info->xs; i<info->xs+info->xm; i++) {
-            x = -2.0 + i * hx;
-            au[j][i] = (*ufcn)(x,y);
-        }
-    }
-    PetscCall(DMDAVecRestoreArray(info->da, u, &au));
     return 0;
 }
