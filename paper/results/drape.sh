96d4ec34d9d48d499919967615ad9ae635ed48a1
Ed Bueler <elbueler@alaska.edu>
2023-07-24 15:44:56 -0800

    will run this

--- paper/results/drape.sh
+++ paper/results/drape.sh
@@ -4,7 +4,7 @@ for PROB in ball spiral; do
     echo ""
     echo "******** -nofas PROB=${PROB} *******"
     for LEVS in 1 2 3 4 5 6 7 8; do
-        python3 drape.py -prob $PROB -levs $LEVS -nofas
+        python3 drape.py -prob $PROB -levs $LEVS -nofas -snes_rtol 1.0e-12 -snes_atol 1.0e-12
     done
 done
 
@@ -12,8 +12,8 @@ for CYCLE in multiplicative full; do
     for PROB in ball spiral; do
         echo ""
         echo "******** PROB=${PROB} CYCLE=${CYCLE} *******"
-        for LEVS in 1 2 3 4 5 6 7 8 9 10; do
-            python3 drape.py -prob $PROB -levs $LEVS -fascd_cycle_type $CYCLE
+        for LEVS in 1 2 3 4 5 6 7 8 9 10 11; do
+            python3 drape.py -prob $PROB -levs $LEVS -fascd_cycle_type $CYCLE -fascd_rtol 1.0e-12 -fascd_atol 1.0e-12
         done
     done
 done
