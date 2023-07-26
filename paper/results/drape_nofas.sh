f0c8ed5f8024238ce216e39c13b6a999a414b7ed
Ed Bueler <elbueler@alaska.edu>
2023-07-25 20:41:29 -0800

    using fascd branch elbueler/stol, drape results look good

--- paper/results/drape_nofas.sh
+++ paper/results/drape_nofas.sh
@@ -4,6 +4,6 @@ for PROB in ball spiral; do
     echo ""
     echo "******** -nofas PROB=${PROB} *******"
     for LEVS in 1 2 3 4 5 6 7 8; do
-        python3 drape.py -prob $PROB -levs $LEVS -nofas -snes_rtol 1.0e-12 -snes_atol 1.0e-12
+        python3 drape.py -prob $PROB -levs $LEVS -nofas -snes_rtol 1.0e-12
     done
 done
