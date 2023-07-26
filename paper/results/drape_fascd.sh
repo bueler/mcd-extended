b6b7ebc1d94968dbe721eb2a0a2f07cc46623874
Ed Bueler <elbueler@alaska.edu>
2023-07-26 11:38:30 -0800

    WIP: updating Example 8.1 results

--- paper/results/drape_fascd.sh
+++ paper/results/drape_fascd.sh
@@ -1,17 +1,19 @@
 # generate FASCD results for Example 8.1 in paper
+# warning: the level 10,11 runs assume decent memory on you machine
+# note: these runs are serial so as to fix the smoother
 
 for PROB in ball spiral; do
     echo ""
     echo "******** PROB=${PROB}, V-cycles, tight tolerance (over-solving) *******"
-    for LEVS in 1 2 3 4 5 6 7 8 9 10; do
+    for LEVS in 1 2 3 4 5 6 7 8 9 10 11; do
         python3 drape.py -prob $PROB -levs $LEVS -fascd_cycle_type multiplicative -fascd_rtol 1.0e-12
     done
 done
 
 for PROB in ball spiral; do
     echo ""
-    echo "******** PROB=${PROB}, F-cycles, to discretization error *******"
-    for LEVS in 1 2 3 4 5 6 7 8 9 10; do
+    echo "******** PROB=${PROB}, F-cycles, to discretization error, with default rtol 1.0e-8 *******"
+    for LEVS in 1 2 3 4 5 6 7 8 9 10 11; do
         python3 drape.py -prob $PROB -levs $LEVS -fascd_cycle_type full
     done
 done
