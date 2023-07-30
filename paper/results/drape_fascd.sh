cddcbaa8a264771d0ccb9afe4564066bb883fda9
Ed Bueler <elbueler@alaska.edu>
2023-07-29 16:02:29 -0800

    WIP: compare to G&K, and asymp rates figure; waiting on last re-run

--- paper/results/drape_fascd.sh
+++ paper/results/drape_fascd.sh
@@ -6,14 +6,14 @@ for PROB in ball spiral; do
     echo ""
     echo "******** PROB=${PROB}, V-cycles, tight tolerance (over-solving) *******"
     for LEVS in 1 2 3 4 5 6 7 8 9 10 11; do
-        python3 drape.py -prob $PROB -levs $LEVS -fascd_cycle_type multiplicative -fascd_rtol 1.0e-12
+        python3 drape.py -prob $PROB -levs $LEVS -fascd_cycle_type multiplicative -fascd_monitor -fascd_atol 1.0e-12 -fascd_rtol 1.0e-12 -fascd_stol 1.0e-12
     done
 done
 
 for PROB in ball spiral; do
     echo ""
-    echo "******** PROB=${PROB}, F-cycles, to discretization error, with default rtol 1.0e-8 *******"
+    echo "******** PROB=${PROB}, F-cycles, to discretization error, with defaults rtol=stol=1.0e-8 and atol=1.0e-50 *******"
     for LEVS in 1 2 3 4 5 6 7 8 9 10 11; do
-        python3 drape.py -prob $PROB -levs $LEVS -fascd_cycle_type full
+        python3 drape.py -prob $PROB -levs $LEVS -fascd_cycle_type full -fascd_monitor
     done
 done
