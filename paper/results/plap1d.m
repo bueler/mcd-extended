5e357fdcd772898537948e60245379756d860110
Ed Bueler <elbueler@alaska.edu>
2023-08-18 13:45:10 -0800

    Fix bug in Example 8.2.
    The error is seen in the unregularized full cycle results
    in the old plap1d.txt file.  Some such runs converged
    because of du=0 steps, so _stol was satisfied.
    The regularization is needed, at least for full cycles.
    In these results, which are better than before, all runs
    use -epsreg 1.0-8 (new default) and -fascd_stol 0.

--- paper/results/plap1d.m
+++ paper/results/plap1d.m
@@ -1,17 +1,11 @@
 % generate convergence rates from results in plap1d.txt
+% errors are from default V(1,1) runs
 
 h = 2.^(-(0:9));
-fasterr = [2.263e-01 3.255e-02 9.108e-03 3.248e-03 5.505e-04 ...
-           1.690e-04 4.717e-05 9.522e-06 3.591e-06 4.948e-07];
-%degerr = [8.700e-02 3.812e-02 1.589e-02 6.318e-03 2.330e-03 ...
-%          6.992e-04 2.047e-04 1.078e-04 3.997e-05 1.506e-05];
-%loglog(h,fasterr,'*',h,degerr,'o')
-loglog(h,fasterr,'*')
+err = [2.263e-01 3.255e-02 9.108e-03 3.248e-03 5.501e-04 ...
+       1.684e-04 4.656e-05 9.203e-06 3.430e-06 4.139e-07];
+loglog(h,err,'*')
 grid on,  xlabel h,  ylabel('|error|_{inf}')
-%legend('p=1.5','p=4.0')
 legend('p=1.5')
-fastp = polyfit(log(h),log(fasterr),1)
-%degp = polyfit(log(h),log(degerr),1)
-
-%   fastp = 2.0047  -1.7579
-%   degp  = 1.4203  -2.2802
+p = polyfit(log(h),log(err),1)
+%   p = 2.0047  -1.7579
